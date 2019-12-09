8c5740babb3e75e21f34357d82fc56a94980b6b6
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-12-09 15:35:17 -0600

    'Login and Registration Pages!'

---
+++ login/app.js
@@ -0,0 +1,45 @@
+let loginUsernameTextBox = document.getElementById("loginEmailTextBox");
+let loginPasswordTextBox = document.getElementById("loginPasswordTextBox");
+let loginButton = document.getElementById("loginButton");
+let message = document.getElementById('message')
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("onAuthStateChanged");
+    console.log(user);
+    let signoutButton = document.getElementById("signoutButton");
+    message.innerHTML = `<button type="submit" id="signoutButton" onclick="signOut()" class="btn btn-primary">log out</button>`
+  } else {
+    // User is signed out.
+    // ...
+  }
+});
+loginButton.addEventListener("click", () => {
+event.preventDefault()
+  let email = loginEmailTextBox.value;
+  let password = loginPasswordTextBox.value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      message.innerHTML = errorMessage
+      // ...
+    });
+})
+
+function signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        function() {
+          message.innerHTML = 'Signed Out!'
+        },
+        function(error) {
+          console.error("Sign Out Error", error);
+        }
+      );
+  }
--- registration/app.js
+++ registration/app.js
@@ -3,8 +3,10 @@ let registerPasswordTextBox = document.getElementById(
   "registerPasswordTextBox"
 );
 let registerButton = document.getElementById("registerButton");
+let message = document.getElementById('message')
 
 registerButton.addEventListener("click", () => {
+  event.preventDefault()
   let email = registerEmailTextBox.value;
   let password = registerPasswordTextBox.value;
 
@@ -15,51 +17,8 @@ registerButton.addEventListener("click", () => {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
-      console.log(errorMessage);
+      message.innerHTML = errorMessage
     });
 });
 
-let loginUsernameTextBox = document.getElementById("loginEmailTextBox");
-let loginPasswordTextBox = document.getElementById("loginPasswordTextBox");
-let loginButton = document.getElementById("loginButton");
 
-let signoutButton = document.getElementById("signoutButton");
-signoutButton.addEventListener("click", () => {
-  firebase
-    .auth()
-    .signOut()
-    .then(
-      function() {
-        console.log("Signed Out");
-      },
-      function(error) {
-        console.error("Sign Out Error", error);
-      }
-    );
-});
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    console.log("onAuthStateChanged");
-    console.log(user);
-  } else {
-    // User is signed out.
-    // ...
-  }
-});
-
-loginButton.addEventListener("click", () => {
-  let email = loginEmailTextBox.value;
-  let password = loginPasswordTextBox.value;
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(errorMessage);
-      // ...
-    });
-});
