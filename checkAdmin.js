b7734b9f3b25e3eac5241e1e9a262961e360e9e3
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-12-13 16:19:20 -0600

    fixed check if admin

--- checkAdmin.js
+++ checkAdmin.js
@@ -1,6 +1,5 @@
 firebase.auth().onAuthStateChanged(function(user) {
   if (user.email != "admin1@ticketr.com") {
-    //window.location.href = "index.html";
+    window.location.href = "index.html";
   }
-  console.log(user.email)
 });
