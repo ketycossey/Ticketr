5585f6aa5592d7f76e72065bb90a7feabf69955d
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-12-15 03:00:35 -0600

    Final Front End Changes

--- jb-user.js
+++ jb-user.js
@@ -61,6 +61,7 @@ function emailAdmin(subject, date, description) {
 
 // View all tickets button **** needs to only display tickets for logged-in user
 function viewArchive() {
+  viewing.innerHTML = "<h2 class='text-primary'>Viewing <span class='text-muted'>Archived </span> Tickets</h2>";
   archiveUL.style.cssText = "display: block;";
   setupArchiveObservers();
   allTicketsUL.style.cssText = "display: none";
@@ -93,17 +94,16 @@ function setupArchiveObservers() {
 }
 
 function updateArchiveUI(archiveTickets) {
-  viewing.innerHTML = "<h2 class='text-primary'>Viewing Archived Tickets</h2>";
   let allArchiveTicketsAttributes = archiveTickets.map(
     (archiveTicket, index) => {
       return `
-      <div class="card">
-      <ul class="list-group list-group-flush border border-warning text-muted">
-        <li class="list-group-item bg-light"><b class="text-muted">Subject:</b> ${archiveTicket.Subject}</li>
-        <li class="list-group-item bg-light"><b class="text-muted">Submitted at: </b>${archiveTicket.Date}</li>
-        <li class="list-group-item bg-light"><b class="text-muted">Priority:</b> ${archiveTicket.Priority}</li>
-        <li class="list-group-item bg-light"><b class="text-muted">Description: </b>${archiveTicket.Description}</li>
-        <li class="list-group-item bg-light"><b class="text-muted">Admin Message: </b>${archiveTicket.AdminMessage}</li>
+      <div class="mb-3">
+      <ul class="list-group list-group-flush border-top border-bottom border-secondary text-secondary">
+        <li class="list-group-item bg-light"><b class="text-secondary">Subject:</b> ${archiveTicket.Subject}</li>
+        <li class="list-group-item bg-light"><b class="text-secondary">Submitted at: </b>${archiveTicket.Date}</li>
+        <li class="list-group-item bg-light"><b class="text-secondary">Priority:</b> ${archiveTicket.Priority}</li>
+        <li class="list-group-item bg-light"><b class="text-secondary">Description: </b>${archiveTicket.Description}</li>
+        <li class="list-group-item bg-light"><b class="text-secondary">Admin Message: </b>${archiveTicket.AdminMessage}</li>
       </ul>
     </div>
                `;
@@ -116,7 +116,7 @@ function updateArchiveUI(archiveTickets) {
 
 function setupObservers() {
   allTicketsUL.style.cssText = "display:block";
-  viewing.innerHTML = "<h2 class='text-primary'>Viewing Open Tickets</h2>";
+  viewing.innerHTML = "<h2 class='text-primary'>Viewing <span class='text-success'>All </span>Tickets</h2>";
   archiveUL.style.cssText = "display: none";
   // Finds the logged in user
   firebase.auth().onAuthStateChanged(function(user) {
@@ -138,6 +138,20 @@ function setupObservers() {
       });
     }
   });
+}
+
+function colorStatus() {
+    let statuses = document.getElementsByClassName('status')
+    for(let i = 0; i < statuses.length; i++) {
+      console.log(statuses[i].textContent)
+      if (statuses[i].textContent.includes('Unresolved')) {
+        statuses[i].classList.add('text-muted')
+      } else if (statuses[i].textContent.includes('Resolved')) {
+        statuses[i].classList.add('text-success')
+      } else if (statuses[i].textContent.includes('In Progress')) {
+        statuses[i].classList.add('text-warning')
+    }
+}
 }
 
 // this function changes the status of a ticket in the database to cancelled. button is in updateUI
@@ -176,25 +190,25 @@ function updateUI(allTickets) {
   let allTicketsAttributes = allTickets.map((ticket, index) => {
     return `
 
-    <div class="card">
-    <ul class="list-group list-group-flush border border-primary">
-      <li class="list-group-item bg-light"><b class="text-primary">subject:</b> ${ticket.Subject}</li>
+    <div class="mb-3">
+    <ul class="list-group list-group-flush border-bottom border-top border-primary">
+      <li class="list-group-item bg-light"><b class="text-primary">Subject:</b> ${ticket.Subject}</li>
       <li class="list-group-item bg-light"><b class="text-primary">Submitted at: </b>${ticket.Date}</li>
       <li class="list-group-item bg-light"><b class="text-primary">Priority:</b> ${ticket.Priority}</li>
       <li class="list-group-item bg-light"><b class="text-primary">Description: </b>${ticket.Description}</li>
-      <li class="list-group-item bg-light" id="ticketStatus${index}"><b class="text-primary">Status: </b>${ticket.Status}</li>
+      <li class="list-group-item bg-light status"><b class="text-primary">Status: </b>${ticket.Status}</li>
       <li class="list-group-item bg-light"><b class="text-primary">Message from Admin: </b>${ticket.AdminMessage}</li>
       <li class="list-group-item bg-light"> 
-        <button class='btn btn-light' onclick='sendTicketToArchive(${index})'>Mark as Complete</button>
         <button class='btn btn-light' onclick='emailAdmin("${ticket.Subject}","${ticket.Date}", "${ticket.Description}")'>Send a Message to Admin</button>
+        <button class='btn btn-light' onclick='sendTicketToArchive(${index})'>Mark as Complete</button>
       </li>
     </ul>
-  </div>
+    </div>
 
                `;
   });
   allTicketsUL.innerHTML = allTicketsAttributes.join("");
-  
+  colorStatus()
 }
 
 // adds a ticket to the database upon clicking submit
