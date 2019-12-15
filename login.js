e3b23d8cc9d51dc4f6860ac8864590fcfcf7feca
bayliewarrick <57427446+bayliewarrick@users.noreply.github.com>
2019-12-15 10:58:51 -0600

    added admin navbar option when signed in as admin

--- login.js
+++ login.js
@@ -4,6 +4,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         navItem3.innerHTML = `<a class="nav-link" id="navItem3" href="submitTicket.html">Submit Ticket</a>`;
         signoutButton();
         welcomeMessage(user);
+        adminNavBar(user)
     } else {
         navItem2.innerHTML = `<a class="nav-link" id="navItem2" href="login.html">Log In</a>`;
         navItem3.innerHTML = `<a class="nav-link" id="navItem3" href="register.html">Register</a>`
@@ -18,10 +19,37 @@ function signoutButton() {
         
     }
 }
+
+function signOut() {
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        function() {
+          window.location.href = "index.html";
+        },
+        function(error) {
+          console.error("Sign Out Error", error);
+        }
+      );
+  }
   
 function welcomeMessage(user) {
+    try {
+        let email = user.email
+        let messageContainer = document.getElementById('welcome-message')
+        messageContainer.classList.add('text-primary')
+        messageContainer.innerHTML = `Hi, ${email}`
+    } catch (error) {
+        
+    }
+}
+
+function adminNavBar(user) {
     let email = user.email
-    let messageContainer = document.getElementById('welcome-message')
-    messageContainer.classList.add('text-primary')
-    messageContainer.innerHTML = `Hi, ${email}`
+    console.log(email)
+    if(email == 'admin1@ticketr.com') {
+        navItem2.innerHTML = ''
+        navItem3.innerHTML = `<a class="nav-link"  href="jb-admin.html">Admin Panel</a>`
+    }
 }
